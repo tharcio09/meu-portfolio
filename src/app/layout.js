b5391cbd64796932d69ab6ea0d181ec8c44b54d0
tharcio09 <tharcio09@hotmail.com>
2025-10-19 06:04:34 -0300

    Adicionar Web Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ThemeProvider } from './components/ThemeProvider';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
