ec6e504769fd4c88c58db3be9fc848276257bd52
Tharcio Santos <tharcio09@hotmail.com>
2026-03-02 22:55:25 -0300

    Ajustar o vercel analytics e dependencias no package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import About from './components/sections/About';
 import Experience from './components/sections/Experience';
 import Projects from './components/sections/Projects';
 import Contact from './components/sections/Contact';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Experience />
       <Projects />
       <Contact />
+      <Analytics/>
     </div>
   );
 }
