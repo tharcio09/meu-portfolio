6047fff95b472cf289cc5e72febafc53c739be11
Tharcio Santos <tharcio09@hotmail.com>
2026-05-01 20:57:58 -0300

    chore: configura eslint e prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  {
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      'out/**',
+      'build/**',
+      'coverage/**',
+      'next-env.d.ts',
+    ],
+  },
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+];
+
+export default eslintConfig;
