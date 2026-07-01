21233cdbb86719bba8ba71dc9043d95d20c35363
Tharcio Santos <tharcio09@hotmail.com>
2026-07-01 17:23:34 -0300

    refactor(scroll-reveal): remover ScrollReveal de todas as seções
    - Remover wrapper ScrollReveal de Projetos, Processo, Competências,
      Sobre mim, Experiência e Contato
    - Remover importação do ScrollReveal do page.tsx
    - Componentes mantêm suas próprias animações internas de intersection observer
    
    ScrollReveal em todas as seções criava um efeito landing page onde
    o conteúdo desliza ao rolar. Seções de conteúdo devem ser visíveis
    imediatamente para escaneamento rápido.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,17 @@ import Projects from './components/sections/Projects';
 import Process from './components/sections/Process';
 import Contact from './components/sections/Contact';
 import { ClientAnalytics } from './components/ClientAnalytics';
-import ScrollReveal from './components/ui/ScrollReveal';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
-      <ScrollReveal>
-        <Projects />
-      </ScrollReveal>
-      <ScrollReveal>
-        <Process />
-      </ScrollReveal>
-      <ScrollReveal>
-        <Capabilities />
-      </ScrollReveal>
-      <ScrollReveal>
-        <About />
-      </ScrollReveal>
-      <ScrollReveal>
-        <Experience />
-      </ScrollReveal>
-      <ScrollReveal>
-        <Contact />
-      </ScrollReveal>
+      <Projects />
+      <Process />
+      <Capabilities />
+      <About />
+      <Experience />
+      <Contact />
       <ClientAnalytics />
     </div>
   );
